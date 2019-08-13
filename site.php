a67f7fbf2124bca6812c896e62736574ec4d8ab7
adminis <yi_xing_hua@163.com>
2019-08-13 20:58:02 +0800

    update

--- site.php
+++ site.php
@@ -9,25 +9,23 @@ defined('IN_IA') or exit('Access Denied');
 
 class Event_registrationModuleSite extends WeModuleSite {
 
-    public function doMobileIndex() {
-        //这个操作被定义用来呈现 功能封面
+
+	public function doMobileIndex() {
+		//这个操作被定义用来呈现 功能封面
         global $_W, $_GPC;
+        echo "<a href='" . $this->createMobileUrl('index') . "'>clickme</a>";
+        echo "<a href='google.com'>clickme</a>";
         include $this->template('index');
-    }
-
-    public function doWebIndex() {
-        //这个操作被定义用来呈现 管理中心导航菜单
-        global $_W, $_GPC;
-        include $this->template('index');
-    }
-
-    public function doWebList() {
-        //这个操作被定义用来呈现 管理中心导航菜单
-    }
-
-    public function doWebDetails() {
-        //这个操作被定义用来呈现 管理中心导航菜单
-    }
-
+	}
+	public function doWebIndex() {
+		//这个操作被定义用来呈现 管理中心导航菜单
+	}
+		public function doWebList() {
+		//这个操作被定义用来呈现 管理中心导航菜单
+	}
+		public function doWebDetails() {
+		//这个操作被定义用来呈现 管理中心导航菜单
+	}
+	
 
 }
